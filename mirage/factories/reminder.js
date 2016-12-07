cec4cd111abba8a4047f8130d7512be811fbabeb
Ian Lancaster <ianclancaster@gmail.com>
2016-12-07 14:58:47 -0700

    Change reminder factory to use compatable filler text

--- mirage/factories/reminder.js
+++ mirage/factories/reminder.js
@@ -1,7 +1,7 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  title: () => faker.hacker.noun(),
+  title: () => faker.hacker.noun().capitalize(),
   date: () => faker.date.recent(3),
   notes: () => faker.hacker.phrase(),
   pinned: false
