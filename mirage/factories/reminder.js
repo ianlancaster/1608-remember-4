1fd72539cd9c955a6f6425309b51f578c330380b
Alex Pilewski <pilewski.alex@gmail.com>
2016-12-08 02:59:03 -0700

    Update faker generator (again)

--- mirage/factories/reminder.js
+++ mirage/factories/reminder.js
@@ -1,7 +1,7 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  title: () => faker.random.words(),
+  title: () => faker.hacker.noun().capitalize(),
   date: () => faker.date.recent(3),
   notes: () => faker.hacker.phrase(),
   pinned: false
