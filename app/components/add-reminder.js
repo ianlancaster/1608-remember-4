b24c6a76c7f2b62e8786aadd8436a7ef3aad63e3
Alex Pilewski <pilewski.alex@gmail.com>
2016-12-08 15:26:16 -0700

    add new reminder

--- app/components/add-reminder.js
+++ app/components/add-reminder.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   store: Ember.inject.service(),
 
   tagName: 'section',
-  classNames: ['add-grocery'],
+  classNames: ['add-reminder'],
 
   title: '',
   date: '',
