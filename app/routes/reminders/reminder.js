7978ead98ae467d1ce731d6eb3a0bc1882a22227
Ian Lancaster <ianclancaster@gmail.com>
2016-12-08 18:32:32 -0700

    lints all project files

--- app/routes/reminders/reminder.js
+++ app/routes/reminders/reminder.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.find('reminder', params.reminder_id)
+    return this.store.find('reminder', params.reminder_id);
   }
 });
