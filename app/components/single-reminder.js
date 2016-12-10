ef7b2cbc69b12134e5de15bcb872af4d74be67fd
Ian Lancaster <ianclancaster@gmail.com>
2016-12-10 10:30:22 -0700

    implement working patch for edit reminder

--- app/components/single-reminder.js
+++ app/components/single-reminder.js
@@ -6,40 +6,15 @@ export default Ember.Component.extend({
   tagName: 'section',
   classNames: ['reminder'],
 
-  title: '',
-  date: '',
-  notes: '',
-
   actions: {
-    createReminder() {
-      const reminder = this.getProperties('title', 'date', 'notes');
-      reminder.date = new Date(reminder.date);
-      this.get('store').createRecord('reminder', reminder).save().then(() => {
-        this.setProperties({ title: '', date: '', notes: '' });
-      });
-    },
-
     editReminder(reminder) {
       reminder.set('isEditing', true);
     },
 
-    // saveReminder(reminder) {
-    //   reminder.save().then(
-    //     reminder => reminder.set('isEditing', false)
-    //   );
-    // },
-
-    saveReminder(oldReminder) {
-      const reminder = oldReminder.getProperties('title', 'date', 'notes');
-      reminder.date = new Date(reminder.date);
-      this.get('store').createRecord('reminder', reminder).save().then(() => {
-        this.setProperties({ title: '', date: '', notes: '' });
-      });
-      oldReminder.destroyRecord();
+    saveReminder(reminder) {
+      reminder.save().then(
+        reminder => reminder.set('isEditing', false)
+      );
     },
-
-    deleteReminder(reminder) {
-      reminder.destroyRecord();
-    }
   }
 });
