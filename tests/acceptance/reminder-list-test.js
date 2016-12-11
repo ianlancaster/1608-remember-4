fdfc6c28c64a841d31d33bc0fcd392340ebf6f4a
Alex Pilewski <pilewski.alex@gmail.com>
2016-12-11 14:25:55 -0700

    Add acceptance test for default page

--- tests/acceptance/reminder-list-test.js
+++ tests/acceptance/reminder-list-test.js
@@ -7,10 +7,22 @@ import Ember from 'ember';
 
 moduleForAcceptance('Acceptance | reminders list');
 
+test('viewing the default page without reminders', function(assert) {
+  server.createList('reminder', 0);
+
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/reminders');
+    assert.equal(Ember.$('.welcome-message').length, 1);
+  });
+});
+
 test('viewing the homepage', function(assert) {
   server.createList('reminder', 5);
 
   visit('/');
+
 
   andThen(function() {
     assert.equal(currentURL(), '/reminders');
