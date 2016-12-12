2ed1c5818b821e62d90dd4101a0533aaa6da016e
Ian Lancaster <ianclancaster@gmail.com>
2016-12-12 12:20:06 -0700

    write acceptance test for editing reminder

--- tests/acceptance/single-reminder-test.js
+++ tests/acceptance/single-reminder-test.js
@@ -12,15 +12,12 @@ moduleForAcceptance('Acceptance | single reminder', {
   });
 
   test('it should add a new reminder to the rendered list when the add reminder field is submitted with values', function(assert) {
-  visit('/reminders/new');
-  fillIn('.spec-input-title', 'sample title');
-  fillIn('.spec-input-date', '11/19/2016');
-  fillIn('.spec-textarea-notes', 'sample notes');
+    visit('/reminders');
+    click('.reminder-edit:first');
+    fillIn('.spec-input-title', 'sample title');
+    click('.add-reminder--submit');
 
-  click('.add-reminder--submit');
-
-  andThen(function() {
-    assert.equal(Ember.$('.reminder-title:last').text().trim(), 'sample title');
-    assert.equal(Ember.$('.spec-input-title').text().trim(), '');
-  });
+    andThen(function() {
+      assert.equal(Ember.$('.reminder-title:first').text().trim(), 'sample title');
+    });
   });
